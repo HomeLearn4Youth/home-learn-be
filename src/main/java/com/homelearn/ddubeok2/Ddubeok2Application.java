8eae334f8c6e42c6b4d57a22d5229c784f485b0a
dhmonukim24 <dhmonukim24@gmail.com>
2023-10-31 16:44:16 +0900

    init: 레포지토리 생성

---
+++ src/main/java/com/homelearn/ddubeok2/Ddubeok2Application.java
@@ -0,0 +1,13 @@
+package com.homelearn.ddubeok2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ddubeok2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ddubeok2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homelearn/ddubeok2/Ddubeok2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homelearn.ddubeok2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ddubeok2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
