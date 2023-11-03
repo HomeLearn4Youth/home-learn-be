205800701158b4df5a97961975b280edba10f4d6
gudals <kimtuti@naver.com>
2023-11-03 13:46:09 +0900

    fix : 공지사항 기능 수정

--- src/main/java/com/homelearn/ddubeok2/notice/Notice.java
+++ src/main/java/com/homelearn/ddubeok2/notice/Notice.java
@@ -1,5 +1,10 @@
 package com.homelearn.ddubeok2.notice;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Notice {
     Long id;
     String title;
--- src/main/java/com/homelearn/ddubeok2/notice/dto/NoticeForm.java
+++ src/main/java/com/homelearn/ddubeok2/notice/dto/NoticeForm.java
@@ -1,5 +1,10 @@
 package com.homelearn.ddubeok2.notice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NoticeForm {
     Long noticeId;
     String title;
