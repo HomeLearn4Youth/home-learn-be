91ca4e2fd45c504fce347df9b4c6d3fbfb1ea9e8
gudals <kimtuti@naver.com>
2023-11-03 12:24:14 +0900

    feat : 공지사항 기능

--- src/main/java/com/homelearn/ddubeok2/notice/Notice.java
+++ src/main/java/com/homelearn/ddubeok2/notice/Notice.java
@@ -1,4 +1,10 @@
 package com.homelearn.ddubeok2.notice;
 
 public class Notice {
+    Long id;
+    String title;
+    String content;
+    int viewCount;
+    String creationTime;
+    Long writerId;
 }
---
+++ src/main/java/com/homelearn/ddubeok2/notice/dto/NoticeForm.java
@@ -0,0 +1,8 @@
+package com.homelearn.ddubeok2.notice.dto;
+
+public class NoticeForm {
+    Long noticeId;
+    String title;
+    String content;
+    Long writerId;
+}
