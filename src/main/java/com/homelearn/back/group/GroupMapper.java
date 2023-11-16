f99107e09e3a9004f433211c1faae9d5e8913030
gudals <kimtuti@naver.com>
2023-11-17 03:27:48 +0900

    feat(group) : 그룹 조회 기능 추가

--- src/main/java/com/homelearn/back/group/GroupMapper.java
+++ src/main/java/com/homelearn/back/group/GroupMapper.java
@@ -3,6 +3,7 @@ package com.homelearn.back.group;
 import com.homelearn.back.group.dto.GroupParam;
 import com.homelearn.back.group.dto.GroupItemInputSpec;
 import com.homelearn.back.group.entity.Group;
+import com.homelearn.back.group.entity.GroupItem;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,7 +13,9 @@ public interface GroupMapper {
 
     void addGroup(GroupParam param);
     void deleteGroup(Long groupId);
+    Group findGroup(Long groupId);
     List<Group> findGroupListByUserId(Long groupId);
     void addGroupItem(GroupItemInputSpec groupItemInputSpec);
+    GroupItem findGroupItem(GroupItemInputSpec groupItemInputSpec);
     void deleteGroupItem(GroupItemInputSpec groupItemInputSpec);
 }
