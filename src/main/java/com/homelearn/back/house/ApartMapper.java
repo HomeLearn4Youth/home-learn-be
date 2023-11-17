9d1a42737452e096bb351df2f8574901b7c07531
김형민 <rlagudals0420@naver.com>
2023-11-17 10:53:16 +0900

    feat(House) : MessageUtil 사용하도록 변경

--- src/main/java/com/homelearn/back/house/ApartMapper.java
+++ src/main/java/com/homelearn/back/house/ApartMapper.java
@@ -9,13 +9,14 @@ import com.homelearn.back.house.entity.HouseJoinLike;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface ApartMapper {
     //동 코드별 아파트
     List<HouseJoinLike> getApartList(ApartListParam listParam);
     //아파트 개별 조회
-    HouseJoinLike getApartInfoById(ApartInfoParam infoParam);
+    Optional<HouseJoinLike> getApartInfoById(ApartInfoParam infoParam);
     //아파트 거래내역 조회
     List<DealListOutputSpec> getApartDealList(DealListInputSpec dealListInputSpec);
 }
